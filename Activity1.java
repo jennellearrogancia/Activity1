04cf234ddce070c5ddc7bf2685dd3ba509a6a1fd
jennellelarrogancia <163534281+jennellearrogancia@users.noreply.github.com>
2024-06-02 17:31:17 +1030

    Create Activity1.java

---
+++ Activity1.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.activity1;
+
+/**
+ *
+ * @author jenne
+ */
+public class Activity1 {
+
+    public static void main (String []args){
+        Salary s = new Salary ();
+        s.setVisible(true);
+        
+        CalculateSal sal = new CalculateSal();
+        sal.setVisible(false);
+    }
+}
